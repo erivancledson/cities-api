52abb07aa72771c427148846c4a9e16647d29d77
Erivan Cledson <erivancled@hotmail.com>
2022-04-15 23:45:38 -0300

    alterado o controller de cidades

--- src/main/java/com/erivan/citiesapi/cities/repositories/CityRepository.java
+++ src/main/java/com/erivan/citiesapi/cities/repositories/CityRepository.java
@@ -7,11 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    //ativar no postman a geocalizacao
-    // CREATE EXTENSION cube;
-    //    CREATE EXTENSION earthdistance;
-
-    @Query(value = "SELECT ((SELECT lat_lon FROM cidade WHERE id=?1) <@> (SELECT lat_lon FROM cidade WHERE id=?2)) as distance", nativeQuery = true)
+     @Query(value = "SELECT ((SELECT lat_lon FROM cidade WHERE id=?1) <@> (SELECT lat_lon FROM cidade WHERE id=?2)) as distance", nativeQuery = true)
     Double distanceByPoints(final Long cityId1, final Long cityId2);
 
     @Query(value = "SELECT earth_distance(ll_to_earth(?1,?2), ll_to_earth(?3,?4)) as distance", nativeQuery = true)
